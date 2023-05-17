2684ed9ac1b01033e3fa748ee04df1f5b6ff38d8
giert <fcgiertsen@gmail.com>
2023-05-17 23:03:35 +0200

    import contacts

--- app/src/main/java/com/example/fon/ContactsViewModel.kt
+++ app/src/main/java/com/example/fon/ContactsViewModel.kt
@@ -1,9 +1,11 @@
-import androidx.compose.runtime.mutableStateOf
+import android.content.Context
+import android.provider.ContactsContract
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fon.Contact
 
 class ContactsViewModel : ViewModel() {
-    private val _contacts = mutableStateOf(listOf<Contact>())
+    private val _contacts = MutableLiveData(listOf<Contact>())
     val contacts = _contacts
 
     init {
@@ -15,4 +17,35 @@ class ContactsViewModel : ViewModel() {
         )
         _contacts.value = sampleContacts
     }
+
+    fun fetchContacts(context: Context) {
+        val contacts = mutableListOf<Contact>()
+        val contentResolver = context.contentResolver
+        val cursor = contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"
+        )
+
+        if (cursor != null && cursor.moveToFirst()) {
+            val idIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID)
+            val nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+            val phoneNumberIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+
+            do {
+                val id = if (idIndex >= 0) cursor.getInt(idIndex) else -1
+                val name = if (nameIndex >= 0) cursor.getString(nameIndex) else "Unknown"
+                val phoneNumber = if (phoneNumberIndex >= 0) cursor.getString(phoneNumberIndex) else ""
+
+                contacts.add(Contact(id, name, phoneNumber))
+            } while (cursor.moveToNext())
+
+            cursor.close()
+        }
+
+        _contacts.value = contacts
+    }
+
 }
--- app/src/main/java/com/example/fon/MainActivity.kt
+++ app/src/main/java/com/example/fon/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.fon
 
 import ContactListItem
 import ContactsViewModel
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,16 +15,29 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import com.example.fon.ui.theme.FonTheme
-import androidx.lifecycle.viewmodel.compose.viewModel
+import android.Manifest
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: ContactsViewModel by viewModels()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                viewModel.fetchContacts(this)
+            } else {
+                // Show a message to the user if the permission is not granted
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FonTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -30,17 +46,24 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        val permissionStatus =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+        if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.READ_CONTACTS)
+        } else {
+            viewModel.fetchContacts(this)
+        }
     }
-}
 
-@Composable
-fun ContactsListScreen() {
-    val viewModel: ContactsViewModel = viewModel()
-    val contacts = viewModel.contacts.value
+    @Composable
+    fun ContactsListScreen() {
+        val contacts by viewModel.contacts.observeAsState(emptyList())
 
-    LazyColumn {
-        items(contacts) { contact ->
-            ContactListItem(contact)
+        LazyColumn {
+            items(contacts) { contact ->
+                ContactListItem(contact)
+            }
         }
     }
 }
